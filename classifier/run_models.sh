b19d70bfb96f74fc7d4a4990526d33bd7e503241
kaburia <kaburiaaustin1@gmail.com>
2022-09-11 01:28:31 +0300

    first input

---
+++ classifier/run_models.sh
@@ -0,0 +1,8 @@
+# Run the various models
+python train.py --arch resnet
+python train.py --arch densnet
+python train.py --arch alexnet
+
+
+
+# Get the different accuracies and compare model with the highest one
